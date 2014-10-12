7f55d6033dec9eaf7623f5d4a5c74911af786274
Pierre <pierre.besson7@gmail.com>
2014-10-12 23:50:51 +0200

    Remove useless services

--- app/services/administration/serviceDomainTest.js
+++
@@ -1,7 +0,0 @@
-﻿
-function getDomainTest(criteria, pagesInfo) {
-    return Fmk.Helpers.utilHelper.loadLocalData({ Libelle: "Champ libelle 50" });
-}
-module.exports = {
-    get: getDomainTest
-};
