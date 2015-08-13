31d16c5333a7c49385661ca50cad65e047ff1c56
Pierre Besson <pierre.besson7@gmail.com>
2015-08-13 16:34:05 +0200

    [focus-beta] remove

--- update_focus.sh
+++
@@ -1,5 +0,0 @@
-npm install --save focusjs focusjs-components
-cp node_modules/focusjs/dist/focus.js vendor/
-cp node_modules/focusjs-components/dist/js/focus-components.js vendor/
-cp node_modules/focusjs-components/dist/css/focus-components.css vendor/
-cp node_modules/focusjs-components/dist/img/*.{svg,png,gif} app/assets/img
