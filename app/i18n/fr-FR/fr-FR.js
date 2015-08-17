6e5ddb0a0192a7b177a5dc00cba0199df040bdde
Pierre Besson <pierre.besson7@gmail.com>
2015-08-17 15:13:34 +0200

    [translations]

--- app/i18n/fr-FR/fr-FR.js
+++ app/i18n/fr-FR/fr-FR.js
@@ -5,5 +5,19 @@ module.exports = {
         save: "Sauvegarder",
         advancedSearch: "Recherche avancée",
         back: "Retour"
+    },
+    contactInformations: {
+            firstName: 'Nom',
+            lastName: 'Prénom',
+            bio: 'Bio',
+            job: 'Travail'
+    },
+    contactAddress: {
+            line1: 'Nom',
+            zipCode: 'Prénom',
+            country: 'Bio',
+            phone: 'Travail',
+            latitude: 'Latitude',
+            longitude: 'Longitude'
     }
 };
