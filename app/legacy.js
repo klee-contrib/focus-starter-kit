43af34ad73f483715347759caec8634d71b32bfc
Stan <bernardstanislas@gmail.com>
2015-10-06 15:03:21 +0200

    [legacy] Add script

---
+++ app/legacy.js
@@ -0,0 +1,15 @@
+FocusComponents.common.detail.component = FocusComponents.components.ScrollspyContainer;
+FocusComponents.common.block.component = FocusComponents.components.Panel;
+console.warning(`
+
+
+**************************************************
+*****************   Caution !!  ******************
+**************************************************
+
+You are using a legacy namespace, to map old components' namespaces to refactored components.
+Please check in your app all references to FocusComponents.common.detail.component and FocusComponents.common.block.component, and change them to FocusComponents.components.ScrollspyContainer and FocusComponents.components.Panel respectively.
+Thank you for your cooperation and have an awesome day !
+
+
+`);
