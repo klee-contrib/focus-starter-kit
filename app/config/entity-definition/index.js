c76529747a651355e75cfd8520b7e17c91a69688
Thomas <tgallon@gmail.com>
2015-11-04 16:35:17 +0100

    [definition] add movie definitions

--- app/config/entity-definition/index.js
+++ app/config/entity-definition/index.js
@@ -1,7 +1,41 @@
 /**
- * These metadatas are generated automatically.
- * @type {Object}
- */
-module.exports = {
+* These metadatas are generated automatically.
+* @type {Object}
+*/
+export default {
+    movie: {
+        code: {
+            domain: 'DO_ID',
+            required: true
+        },
+        title: {
+            domain: 'DO_LABEL_LONG',
+            required: true
+        },
+        originalTitle: {
+            domain: 'DO_LABEL_LONG',
+            required: false
+        },
+        synopsis: {
+            domain: 'DO_COMMENT',
+            required: true
+        },
+        shortSynopsis: {
+            domain: 'DO_COMMENT',
+            required: true
+        },
+        runtime: {
+            domain: 'DO_LABEL_SHORT', //to change to runtime
+            required: true
+        },
+        movieType: {
+            domain: 'DO_LABEL_SHORT',
+            required: true
+        },
+        productionYear: {
+            domain: 'DO_YEAR',
+            required: true
+        }
+    }
 
 };
