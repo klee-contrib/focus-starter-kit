1a9acb07530c2ac51261fd50e8e273444a25a042
Thomas <tgallon@gmail.com>
2015-11-17 18:28:00 +0100

    [person] add actions on person

--- app/views/person/detail/person-identity.jsx
+++ app/views/person/detail/person-identity.jsx
@@ -4,18 +4,32 @@ import FocusComponents from 'focus-components';
 
 // web components
 import Panel from 'focus-components/components/panel';
+import {mixin as formMixin} from 'focus-components/common/form';
+
+//stores & actions
+import personStore from '../../../stores/person';
+import {identityActions} from '../../../action/person';
 
 export default React.createClass({
-    displayName: 'PersonIdentity',
+    displayName: 'PersonIdendity',
     propTypes: {
         id: PropTypes.number
     },
+    mixins: [formMixin],
+    definitionPath: 'person',
+    stores: [{store: personStore, properties: ['person']}],
+    action: identityActions,
 
     /** @inheritDoc */
-    render() {
+    renderContent() {
         return (
-            <Panel title='person.detail.identity'>
-                Ici l'identité.
+            <Panel actions={this._renderActions} title='person.detail.identity'>
+                {this.fieldFor('fullName')}
+                {this.fieldFor('firstName')}
+                {this.fieldFor('sex')}
+                {this.fieldFor('birthDate')}
+                {this.fieldFor('birthPlace')}
+                {this.fieldFor('activity')}
             </Panel>
         );
     }
