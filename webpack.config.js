21a3e760ad36d6446f7ed83edca06f14ee392f0f
Stan <bernardstanislas@gmail.com>
2016-01-06 15:44:38 +0100

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
 const configBuilder = require('webpack-focus').configBuilder;
 const path = require('path');
 // Check if focus libraries should be held locally or read from NPM
-const localFocus = process.env.LOCAL_FOCUS;
+const localFocus = process.env.LOCAL_FOCUS ? JSON.parse(process.env.LOCAL_FOCUS) : false;
 const customConfig = localFocus ? {
     resolve: {
         alias: {
-            'focus-core': path.resolve(process.cwd(), '../focus-core/lib'),
-            'focus-components': path.resolve(process.cwd(), '../focus-components/lib'),
+            'focus-core': path.resolve(process.cwd(), '../focus-core'),
+            'focus-components': path.resolve(process.cwd(), '../focus-components'),
             react: path.resolve(process.cwd(), './node_modules/react'),
             backbone: path.resolve(process.cwd(), './node_modules/backbone'),
             moment: path.resolve(process.cwd(), './node_modules/moment'),
             'i18next-client': path.resolve(process.cwd(), './node_modules/i18next-client')
-        },
-        extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx']
+        }
     }
 } : {};
 module.exports = configBuilder(customConfig);
