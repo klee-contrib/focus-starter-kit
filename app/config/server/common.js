1ef812304704b1b89c8f9cdc054a2c1c641df694
Thomas <thomas.gallon@gmail.com>
2016-01-06 23:21:17 +0100

    correct common root since rebase and new webpack

--- app/config/server/common.js
+++ app/config/server/common.js
@@ -1,8 +1,8 @@
 import builder from 'focus-core/util/url/builder';
 
-const searchRoot = API_ROOT + '/common/';
+const commonRoot = './common/';
 
 export default {
     /* loads */
-    search: builder(searchRoot + 'search?listState.skip=${skip}&listState.top=${top}', 'POST')
+    search: builder(commonRoot + 'search?listState.skip=${skip}&listState.top=${top}', 'POST')
 };
