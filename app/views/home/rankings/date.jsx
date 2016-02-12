7a6fda4d99527cedc7fac09f4cf7c704d403cbed
bernardstanislas <bernardstanislas@gmail.com>
2016-02-12 10:55:18 +0100

    Purify components

--- app/views/home/rankings/date.jsx
+++ app/views/home/rankings/date.jsx
@@ -1,20 +1,14 @@
 import React, {Component} from 'react';
 import MovieCard from './movie-card';
-import TranslationBehaviour from 'focus-components/behaviours/translation';
+import {translate} from 'focus-core/translation';
 
-@TranslationBehaviour
-class DateRanking extends Component {
-    render() {
-        const {dateRanking} = this.props;
-        return (
-            <div data-role='ranking'>
-                <h1>{this.i18n('movie.rankings.latest.title')}</h1>
-                <div data-role='cards'>
-                    {dateRanking.map(movie => <MovieCard key={movie.code} {...movie}/>)}
-                </div>
-            </div>
-        );
-    }
-}
+const DateRanking = ({dateRanking}) => (
+    <div data-role='ranking'>
+        <h1>{translate('movie.rankings.latest.title')}</h1>
+        <div data-role='cards'>
+            {dateRanking.map(movie => <MovieCard key={movie.code} {...movie}/>)}
+        </div>
+    </div>
+);
 
 export default DateRanking;
--- app/views/home/rankings/mark.jsx
+++ app/views/home/rankings/mark.jsx
@@ -1,20 +1,14 @@
 import React, {Component} from 'react';
 import MovieCard from './movie-card';
-import TranslationBehaviour from 'focus-components/behaviours/translation';
+import {translate} from 'focus-core/translation';
 
-@TranslationBehaviour
-class MarkRanking extends Component {
-    render() {
-        const {markRanking} = this.props;
-        return (
-            <div data-role='ranking'>
-                <h1>{this.i18n('movie.rankings.best.title')}</h1>
-                <div data-role='cards'>
-                    {markRanking.map(movie => <MovieCard key={movie.code} {...movie}/>)}
-                </div>
-            </div>
-        );
-    }
-}
+const MarkRanking = ({markRanking}) => (
+    <div data-role='ranking'>
+        <h1>{translate('movie.rankings.best.title')}</h1>
+        <div data-role='cards'>
+            {markRanking.map(movie => <MovieCard key={movie.code} {...movie}/>)}
+        </div>
+    </div>
+);
 
 export default MarkRanking;
