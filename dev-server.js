d6bcd51c0f83dc5be63c850a73c6cdafc2c064d0
GuenoleK <kikabouguenole@gmail.com>
2016-02-17 16:49:19 +0100

    Add opener:)

--- dev-server.js
+++ dev-server.js
@@ -1,3 +1,7 @@
+const opener = require('opener');
 const webpackConfig = require('./webpack.config');
 const serverLauncher = require('webpack-focus').serverLauncher;
 serverLauncher(webpackConfig);
+const host = process.env.DEV_SERVER_HOST || 'localhost';
+const port = process.env.DEV_SERVER_PORT || 3000;
+opener(`http://${host}:${port}`);
