75ed6340c3541d86134e5c5380fa2b629dcb18d6
Hartorn <bazirehoussin@gmail.com>
2016-11-25 22:05:18 +0100

    Correcting dev tools, adding it by default

--- app/components/app-layout.js
+++ app/components/app-layout.js
@@ -3,16 +3,18 @@ import Layout from 'focus-components/components/layout';
 
 import MenuLeft from '../views/menu/menu-left';
 import Footer from '../views/footer';
+import DevTools from './dev-tools';
 
 const CustomLayout = (props) => (
-        <div>
-          <Layout
-              Footer={Footer}
-              MenuLeft={MenuLeft}
-          >
-          {props.children}
-          </Layout>
-        </div>
+  <div>
+    <Layout
+      Footer={Footer}
+      MenuLeft={MenuLeft}
+      >
+      {props.children}
+    </Layout>
+    {__DEV__ && <DevTools />}
+  </div >
 );
 CustomLayout.displayName = 'CustomAppLayout';
 
--- app/components/dev-tools.js
+++ app/components/dev-tools.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import history from 'focus-core/history';
+import { browserHistory as history } from 'react-router';
 import CoreStore from 'focus-core/store/CoreStore';
 import FocusDevTools from 'focus-devtools';
 const DevTools = props => {
     return (
-      <FocusDevTools
-        isPanel={true} /* If you want to embed the component in a DOck */
-        toggleVisibilityKey='ctrl-m'  /*How do you want to display the dev tool*/
-        routes={history.handlers}  /* A list of all your routes (`focus-core/router/history`)*/
-        getStores={() => CoreStore.prototype._instances} /* A list of all your stores (`focus-core/CoreStore._instances`)*/
-        isDebugDevTools={false} /* If you want to display the dev tools props (not usefull for the projects)*/
-        />
+        <FocusDevTools
+            isPanel={true} /* If you want to embed the component in a DOck */
+            toggleVisibilityKey='ctrl-m'  /*How do you want to display the dev tool*/
+            routes={history.handlers}  /* A list of all your routes (`focus-core/router/history`)*/
+            getStores={() => CoreStore.prototype._instances} /* A list of all your stores (`focus-core/CoreStore._instances`)*/
+            isDebugDevTools={false} /* If you want to display the dev tools props (not usefull for the projects)*/
+            />
     );
 }
 
 DevTools.displayName = 'DevTools';
 
-export default DevTools;
+export default DevTools; 
