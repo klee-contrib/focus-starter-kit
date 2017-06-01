93fa8d1b2c13a05aa277465f5cee3daabc1f0d7b
Hartorn <bazirehoussin@gmail.com>
2017-06-01 09:30:08 +0200

    Updating version, updating config

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,10 @@ const baseConfig = require('webpack-focus/config/default');
 const envParser = require('webpack-focus/webpack-utilities/env-parser');
 
 const parsedConf = envParser(process.env);
-module.exports = baseConfig(process.env, {}).toWebpackConfig(parsedConf);
+
+const myConfig = baseConfig(process.env, {});
+myConfig.addAlias('react', './node_modules/react');
+myConfig.addAlias('react-dom', './node_modules/react-dom');
+myConfig.addAlias('material-design-lite', './node_modules/material-design-lite');
+
+module.exports = myConfig.toWebpackConfig(parsedConf);
